687704629edcd437702dbab410c131f486311a34
JerinXplore <jerinjerin101325@gmail.com>
2026-02-07 22:16:09 +0600

    feat(speciality):created route,controller,service for speciality model api

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express, { Application, Request, Response } from "express";
+import { prisma } from "./app/lib/prisma";
+import { SpecialityRoutes } from "./app/module/speciality/speciality.route";
+import { IndexRoutes } from "./app/routes";
 
 const app : Application = express();
 
@@ -8,9 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.use("/api/v1",IndexRoutes);
+
 // Basic route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript + Express!');
+app.get('/', async (req: Request, res: Response) => {
+  const speciality = await prisma.speciality.upsert({
+  where: { title: 'Cardiology' },
+  update: {},
+  create: { title: 'Cardiology' },
+});
+  res.status(201).json({
+      success: true,
+      messsage: 'API is working',
+      data: speciality
+  })
 });
 
 export default app;
---
+++ src/app/module/speciality/speciality.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { SpecialityController } from "./speciality.controller";
+
+const router = Router();
+
+router.post('/', SpecialityController.createSpeciality);
+router.get('/', SpecialityController.createSpeciality);
+
+export const SpecialityRoutes = router;
