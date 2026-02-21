b666a794c74047bb304907a82cf6a85b8683c724
JerinXplore <jerinjerin101325@gmail.com>
2026-02-21 21:41:56 +0600

    feat(auth): added authorization middleware and secured specialty routes

--- src/app/module/speciality/speciality.route.ts
+++ src/app/module/speciality/speciality.route.ts
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { Router } from "express";
 import { SpecialityController } from "./speciality.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.post('/', SpecialityController.createSpeciality);
-router.get('/', SpecialityController.getAllSpecialities);
-router.delete('/:id', SpecialityController.deleteSpecialities);
+router.post('/', checkAuth(Role.ADMIN,Role.SUPER_ADMIN),SpecialityController.createSpeciality);
+router.get('/',SpecialityController.getAllSpecialities);
+router.delete('/:id',checkAuth(Role.ADMIN,Role.SUPER_ADMIN), SpecialityController.deleteSpecialities);
 router.patch('/:id', SpecialityController.updateSpeciality);
 
 export const SpecialityRoutes = router;
