609adf1654a416bb5b90a68ece75e720d0c19565
JerinXplore <jerinjerin101325@gmail.com>
2026-02-26 12:45:56 +0600

    feat(auth): integrated refresh token service with cookie management

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import { AuthService } from "./auth.service";
 import { sendResponse } from "../../shared/sendResponse";
 import status from "http-status";
 import { tokenUtils } from "../../utils/token";
+import AppError from "../../errorHelpers/AppError";
 
 const registerPatient = catchAsync (
     async (req: Request, res: Response)=>{
@@ -66,10 +67,39 @@ const getMe = catchAsync(
         })
     }
 )
+//express e async function e eerror hole normally crash korte pare..tai catchAsync e internally try/catch kori jeta next(errror) e pathay..eki sathe global error handler hanlde kore
+const getNewToken = catchAsync(
+    async (req: Request, res: Response) => {
+        const refreshToken = req.cookies.refreshToken;
+        const betterAuthSessionToken = req.cookies["better-auth.session_token"];//cookies theke token nisi..bracket notation? karon key er modhdhe - ache jeta dot notation e kaj korbena..
+        if (!refreshToken) {
+            throw new AppError(status.UNAUTHORIZED, "Refresh token is missing");
+        }//cokiee te jodi refresh token ta na thake tahole user logged out hobe,malicious req pabe ..401 dibe
+        const result = await AuthService.getNewToken(refreshToken, betterAuthSessionToken);//controller nije token generate korena..just req handle kore..business logic mainly service laye e..
 
+        const { accessToken, refreshToken: newRefreshToken, sessionToken } = result;//result k destructure kortesi.
+//ekhane special jinish ta holo refreshToken: newRefreshToken.. mane result.refreshToken k rename kore newRefreshToken kora hoise..
+//why?? .. as agei ekta refreshToken variable ache jeta cookie theke newa..renamed in order to avoid conflict..
+        tokenUtils.setAccessTokenCookie(res, accessToken);
+        tokenUtils.setRefreshTokenCookie(res, newRefreshToken);
+        tokenUtils.setBetterAuthSessionCookie(res, sessionToken);//abar cookie set keno korlam?? ... karon old token expired and new token browser e store korte hobe..ar egulo  normally HttpOnly,Secure,SameSite,Expiry time soho set hoy..
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "New tokens generated successfully",
+            data: {
+                accessToken,
+                refreshToken: newRefreshToken,
+                sessionToken,
+            },
+        });
+    }
+)
 
 export const AuthController = {
     registerPatient,
     loginUser,
     getMe,
+    getNewToken,
 }
